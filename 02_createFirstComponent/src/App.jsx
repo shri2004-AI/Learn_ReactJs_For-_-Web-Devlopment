85702877af31d1cc3e60ea041737469af36704b7
shri2004-AI <hs.shri2004@gmail.com>
2026-02-23 17:17:55 +0530

    create first component in react App.

---
+++ 02_createFirstComponent/src/App.jsx
@@ -0,0 +1,12 @@
+import KgButton from "./KgButton";
+function App(){
+  return <div>
+    <h1>Hello Coder...</h1>
+    <button>Subscribe</button>
+    <KgButton></KgButton>
+    <KgButton />
+  </div>
+  
+}
+
+export default App;
