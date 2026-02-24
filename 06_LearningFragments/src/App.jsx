d44f24c13b478e54d917ec537397c506a6fa9369
shri2004-AI <hs.shri2004@gmail.com>
2026-02-24 11:38:29 +0530

    In this lecture i am learn about fragments in reactJs.

---
+++ 06_LearningFragments/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+function App() {
+  // return (
+  //   <React.Fragment>
+  //     <h1>Healthy Food</h1>
+  //     <ul class="list-group">
+  //       <li class="list-group-item">Dal</li>
+  //       <li class="list-group-item">Roti</li>
+  //       <li class="list-group-item">Veggies</li>
+  //       <li class="list-group-item">Fruits</li>
+  //       <li class="list-group-item">Milk</li>
+  //     </ul>
+  //   </React.Fragment>
+  // );
+
+  return (
+    <>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Veggies</li>
+        <li class="list-group-item">Fruits</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
